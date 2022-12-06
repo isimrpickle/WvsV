024f7f9e02c3a35145ce80c6df38a4fee22d4fa2
KKalamari <kyriaki.kal.09@gmail.com>
2022-12-06 19:42:25 +0200

    printig the game map

--- WvsV/game_state.cpp
+++ WvsV/game_state.cpp
@@ -2,17 +2,62 @@
 #include <iostream>
 #include "game_state.h"
 #include <string>
+#include <stdlib.h>
 #include <vector>
 
 using namespace std;
 
+string** create_array_for_map(unsigned  short int x, unsigned short y) {
+    string** array_for_map = new string * [x];
+        for (int i = 0; i < y; i++) {
+            array_for_map[i] = new string[y];
+        }
+        for (int i = 0; i < x; i++) {
+            for (int n = 1; n < y; n++) {
+                array_for_map[i][n] = ":__:";
+            }
+        }
+
+        return array_for_map;
+}
+
 void printing_map(unsigned short int x, unsigned short int y) {
-    vector <vector<string>> map(x, vector<string>(y, ":__:"));
+    cout << system("cls") << endl;
+
+    string** array_for_map = create_array_for_map(x, y);
+
+
+    unsigned short int for_obstacles;
+    for_obstacles = (x * y) / 100;
+    for (int i = 0; i <= for_obstacles; i++) {
+        graphics tree(rand() % x, rand() % y, TREE);
+        graphics water(rand() % x, rand() % y, WATER);
+
+        array_for_map[tree.get_x()][tree.get_y()] = " || ";
+        array_for_map[water.get_x()][water.get_y()] = " ~~ ";
+    }
+
     for (int i = 0; i < x; i++) {
         for (int n = 0; n < y; n++) {
-            cout << map[i][n];
+            cout << array_for_map[i][n];
         }
         cout << endl;
     }
+
 }
-void map_create() {};
+
+
+void map_create() {
+    unsigned short int x=0, y=0;
+    while(x* y <= 15) {
+        cout << "Hello! \n Please enter the dimensions you want! \n x: " << endl;
+        cin >> x;
+        cout << " y: " << endl;
+        cin >> y;
+    };
+
+
+    printing_map(x, y);
+ 
+    
+};
--- WvsV/main.cpp
+++ WvsV/main.cpp
@@ -2,13 +2,5 @@
 #include "game_state.h"
 
 int main() {
-    unsigned short int x, y;
-    cout << "Enter the dimensions you wish! x: " << endl;
-    cin >> x;
-    cout << "y: " << endl;
-    cin >> y;
-
-
-    printing_map(x, y);
-
+    map_create();
 }
