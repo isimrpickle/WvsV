770f7d0607cea856f55e433c287c3d761739b0fa
KKalamari <kyriaki.kal.09@gmail.com>
2022-12-06 19:27:12 +0200

    Printing the map for the game

--- WvsV/game_state.h
+++ WvsV/game_state.h
@@ -3,6 +3,8 @@
 
 #include <list>
 #include <vector>
+#include <stdlib.h>
+
 using namespace std;
 
 typedef enum {
@@ -11,17 +13,18 @@ typedef enum {
 
 
 class graphics {
-    static unsigned num_of_graphics;
+    //static unsigned num_of_graphics;
     const unsigned short int x;
     const unsigned short int y;
     const GraphicType type;
 public:
     graphics(unsigned short int x_, unsigned short int y_, GraphicType type_) :x(x_), y(y_), type(type_) {
-        num_of_graphics++;
+       // num_of_graphics++;
     };
-    static unsigned get_num_of_graphics() { return num_of_graphics; };
+    //static unsigned get_num_of_graphics() { return num_of_graphics; };
     const unsigned short int get_y() { return y; };
     const unsigned short int get_x() { return x; };
+    GraphicType get_id(graphics example) { return example.type; };
 };
 
 
--- WvsV/main.cpp
+++ WvsV/main.cpp
@@ -2,13 +2,7 @@
 #include "game_state.h"
 
 int main() {
-    unsigned short int x, y;
-    cout << "Enter the dimensions you wish! x: " << endl;
-    cin >> x;
-    cout << "y: " << endl;
-    cin >> y;
+    map_create();
 
-
-    printing_map(x, y);
 
 }
